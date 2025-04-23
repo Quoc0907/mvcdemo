6f0b28837b84cadded8f6b33cdbec1aa58f8e0df
Quoc0907 <voa962375@gmail.com>
2025-04-23 11:15:35 +0700

    meomayheo

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ return array(
     'assets' => '//PTITCNTT2402/mvcdemo/assets',
     'db' => array(
         'host'     => 'localhost',
-        'name'     => 'ProductDB',
+        'name'     => 'csdl',
         'username' => 'root',
         'password' => ''
     )
